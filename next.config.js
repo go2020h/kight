e73a0111aa1ae98c0f2ed81d9e29e9c1fc78debb
go2020h <go2020.hidane.inc@gmail.com>
2024-11-08 11:56:31 +0900

    update

--- next.config.js
+++ next.config.js
@@ -4,9 +4,18 @@ const nextConfig = {
     esmExternals: 'loose'
   },
   webpack: (config) => {
-    config.externals = [...config.externals, { canvas: "canvas" }]; // required to make pdfjs work
-    return config;
+    config.externals = [...config.externals, { canvas: 'canvas' }] // required to make pdfjs work
+    return config
   },
-};
 
-module.exports = nextConfig;
+  async rewrites() {
+    return [
+      {
+        source: '/delivery/:path*',
+        destination: 'http://160.251.148.143/:path*'
+      }
+    ]
+  }
+}
+
+module.exports = nextConfig
