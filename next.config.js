03a55f027812f6b80ca401cdfd7d4e3673ec59bb
go2020h <go2020.hidane.inc@gmail.com>
2024-11-08 20:31:06 +0900

    update

--- next.config.js
+++ next.config.js
@@ -6,21 +6,6 @@ const nextConfig = {
   webpack: (config) => {
     config.externals = [...config.externals, { canvas: 'canvas' }] // required to make pdfjs work
     return config
-  },
-
-  async rewrites() {
-    return {
-      beforeFiles: [
-        {
-          source: '/delivery',
-          destination: 'http://160.251.148.143/delivery'
-        },
-        {
-          source: '/delivery/:path*',
-          destination: 'http://160.251.148.143/delivery/:path*'
-        }
-      ]
-    }
   }
 }
 
